d9fb46663457844ce43bc4259303b386ac7ccadd
Raven <ravenkoehler@gmail.com>
2021-09-14 10:57:10 -0400

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,61 @@
  for you to use if you need it!
  */
 
+ let createEmployeeRecord = function(row){
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(records) {
+    return records.map(record => createEmployeeRecord(record));
+}
+
+function createTimeInEvent(date) {
+    const record = { 
+        type: "TimeIn",
+        hour: parseInt(date.split(" ")[1], 10),
+        date: date.split(" ")[0]
+    }
+    this.timeInEvents.push(record);
+    return this;
+}
+
+function createTimeOutEvent(date) {
+    const record = { 
+        type: "TimeOut",
+        hour: parseInt(date.split(" ")[1], 10),
+        date: date.split(" ")[0]
+    }
+    this.timeOutEvents.push(record);
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date);
+    const timeOut = this.timeOutEvents.find(event => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(date) {
+    const hours = hoursWorkedOnDate.call(this, date);
+    return hours * this.payPerHour;
+}
+
+function findEmployeeByFirstName(employees, name) {
+    return employees.find(employee => employee.firstName === name);
+}
+
+function calculatePayroll(records) {
+    const wages = records.map(employee => allWagesFor(employee))
+    return reduce.reduce((previous, current) => previous + current);
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
